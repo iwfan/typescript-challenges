97f75f9d484f9aaf5ed53a2f8b8fc2686a5b6787
iwfan <i.wangfancn@gmail.com>
2021-05-23 21:21:52 +0800

    update example

--- src/warm-up/4.class.ts
+++ src/warm-up/4.class.ts
@@ -104,3 +104,40 @@ let Shoe: ShoeCreater = {
 
  // TODO: context concept?
 }
+
+{
+  interface BuildableRequest {
+    data?: object
+    method: 'get' | 'post'
+    url: string
+  }
+
+class RequestBuilder2 {
+  data?: object
+  method?: 'get' | 'post'
+  url?: string
+
+  setData(data: object): this & Pick<BuildableRequest, 'data'> {
+    return Object.assign(this, {data})
+  }
+
+  setMethod(method: 'get' | 'post'): this & Pick<BuildableRequest, 'method'> {
+    return Object.assign(this, {method})
+  }
+
+  setURL(url: string): this & Pick<BuildableRequest, 'url'> {
+    return Object.assign(this, {url})
+  }
+
+  build(this: BuildableRequest) {
+    return this
+  }
+}
+
+new RequestBuilder2()
+  .setURL('bar') // Try removing me!
+  .setMethod('post') // Try removing me!
+  .setData({})
+  .build()
+
+}
